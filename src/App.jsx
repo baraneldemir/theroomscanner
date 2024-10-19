91fc4d4e38cc4936beab71e571514a0004c8afa3
baraneldemir <brneldemirrr@gmail.com>
2024-10-19 15:24:22 +0100

    front end puppeteer pain

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 import './App.css';
 
 export default function App() {
-    const [images, setImages] = useState([]);
-    // const [titles, setTitles] = useState([]);
+    // const [images, setImages] = useState([]);
+    const [titles, setTitles] = useState([]);
     // const [links, setLinks] = useState([]);
     // const [description, setDescription] = useState([]);
     // const [prices, setPrices] = useState([]);
@@ -13,8 +13,8 @@ export default function App() {
     const [location, setLocation] = useState('');
 
     const fetchImages = async (location) => {
-        setImages([]);
-        // setTitles([]);
+        // setImages([]);
+        setTitles([]);
         // setDescription([]);
         // setLinks([]);
         // setPrices([]);
@@ -23,8 +23,8 @@ export default function App() {
 
         try {
             const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/scrape-images/${location}`);
-            setImages(response.data.images);
-            // setTitles(response.data.titles);
+            // setImages(response.data.images);
+            setTitles(response.data.titles);
             // setPrices(response.data.prices);
             // setLinks(response.data.links);
             // setDescription(response.data.description);
@@ -73,18 +73,18 @@ export default function App() {
             )}
             
             {/* Display images and room info after loading */}
-            {!loading && images.length > 0 && (
+            {!loading && titles.length > 0 && (
                 <div className="flex flex-col items-start gap-4 mt-6">
-                    {images.map((image, index) => (
+                    {titles.map((title, index) => (
                         <div key={index} className="flex items-center w-full gap-4 p-5 bg-white shadow-md rounded-xl">
                             {/* <a href={links[index]} target="_blank" rel="noopener noreferrer"> */}
-                                <img src={image} alt={`Room ${index}`} className="rounded-lg shadow-lg w-28 h-28" />
+                                {/* <img src={image} alt={`Room ${index}`} className="rounded-lg shadow-lg w-28 h-28" /> */}
                             {/* </a> */}
-                            {/* <div className='flex flex-col'>
+                            <div className='flex flex-col'>
                                 <span className="text-black">{titles[index] || 'No title available'}</span>
-                                <span className="max-w-md text-xs leading-relaxed text-black break-words">{description[index] || 'No description available'}</span>
-                                <span className="text-black">{prices[index] || 'No price available'}</span>
-                            </div>  */}
+                                {/* <span className="max-w-md text-xs leading-relaxed text-black break-words">{description[index] || 'No description available'}</span>
+                                <span className="text-black">{prices[index] || 'No price available'}</span> */}
+                            </div> 
                         </div>
                     ))}
                 </div>
